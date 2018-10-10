6fdb65ae55ee5d91ceec89cd8637182f0ea66c61
trina <trina_haque28@outlook.com>
2018-10-09 22:38:35 -0700

    Solve two sums using HashMaps

--- Easy/Array/twoSums.py
+++ Easy/Array/twoSums.py
@@ -5,6 +5,7 @@
 # nums = [2, 7, 11, 15], target = 9
 # return [0, 1]
 
+# O(n^2) solution
 def twoSum(nums, target):
     # only one element, return False
     if len(nums) < 2:
@@ -17,5 +18,29 @@ def twoSum(nums, target):
                 return [i, j]
     return False
 
+
+
+
+# O(2n) time solution (one loop to create dict, one loop to loop through the array)
+# O(n) space to store the dict
+def twoSumDict(arr, target):
+    map = {}
+    for i in range(0, len(arr)):
+        map[arr[i]] = i
+    # print (type(map.keys()))
+    result = []
+    for i in range(0, len(arr)):
+        tmp = arr[i]
+        remainder = target - arr[i]
+        # check if the remainder is in the keys and the index is not the current index
+        if remainder in map.keys() and map[remainder] != i:
+            result = [i, map[remainder]]
+            break
+    return result
+
 nums = [8, 11, 3, 17, 1, 15, 5]
-print (twoSum(nums, 9))
+nums2 = [2, 3]
+print(twoSumDict(nums2, 5))
+
+
+print (twoSum(nums2, 4))
