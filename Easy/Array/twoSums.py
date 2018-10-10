e158e807c00e9249924985182b761f64700654cc
trina <trina_haque28@outlook.com>
2018-10-09 22:46:13 -0700

    Revise space and time complexity for twoSum using Hash Map

--- Easy/Array/twoSums.py
+++ Easy/Array/twoSums.py
@@ -5,13 +5,13 @@
 # nums = [2, 7, 11, 15], target = 9
 # return [0, 1]
 
-# O(n^2) solution
+# O(n^2) time solution
+# O(1) space solution
 def twoSum(nums, target):
     # only one element, return False
     if len(nums) < 2:
         return False
-
-    # O n^2 solution
+    
     for i in range(0, len(nums)-1):
         for j in range(i+1, len(nums)):
             if (nums[i] + nums[j]) == target:
@@ -19,19 +19,19 @@ def twoSum(nums, target):
     return False
 
 
-
-
 # O(2n) time solution (one loop to create dict, one loop to loop through the array)
+# Hash look up time complexity is O(1)
 # O(n) space to store the dict
-def twoSumDict(arr, target):
+def twoSumDict(nums, target):
     map = {}
-    for i in range(0, len(arr)):
-        map[arr[i]] = i
-    # print (type(map.keys()))
+    # fill in the map with key value pair
+    for i in range(0, len(nums)):
+        map[nums[i]] = i
+    
     result = []
-    for i in range(0, len(arr)):
-        tmp = arr[i]
-        remainder = target - arr[i]
+    for i in range(0, len(nums)):
+        tmp = nums[i]
+        remainder = target - nums[i]
         # check if the remainder is in the keys and the index is not the current index
         if remainder in map.keys() and map[remainder] != i:
             result = [i, map[remainder]]
