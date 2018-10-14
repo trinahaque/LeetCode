1c32c854c5152935104654207c060116e6479390
trina <trina_haque28@outlook.com>
2018-10-14 15:07:35 -0700

    Modify problem description

--- Easy/LinkedList/deleteNode.py
+++ Easy/LinkedList/deleteNode.py
@@ -5,6 +5,12 @@ Class ListNode:
 # Assumption that the Node value exist in the Linked List
 # No need to check for edge cases when the Node value doesn't exist
 # Node will never equal to tail node
+# At least two nodes in the linked list
+# All values are unique
+# Do not return anything
+
+# Head = [4,5,1,9], node = 5
+# Output [4, 1, 9]
 def deleteNode(self, node):
     node.val = node.next.val
     node.next = node.next.next
