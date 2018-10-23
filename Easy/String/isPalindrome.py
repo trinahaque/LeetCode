ef2748d07e73286855929a481b2794b41198b315
trina <trina_haque28@outlook.com>
2018-10-23 01:44:05 -0700

    Add edge cases to isPalindrome problem

--- Easy/String/isPalindrome.py
+++ Easy/String/isPalindrome.py
@@ -1,29 +1,10 @@
-def isPalindrome(s):
-    # if only one character, it should be true
-    if len(s) < 2:
-        return True
-
-    i = 0
-    j = len(s) - 1
-
-    while i <= j:
-        # if the character is a special character or space
-        if s[i].isalnum() == False and i < j:
-            i += 1
-            continue
-        elif s[j].isalnum() == False and i < j:
-            j -= 1
-            continue
-        if s[i].lower() != s[j].lower():
-            return False
-        i += 1
-        j -= 1
-    return True
-
 # Time Complexity: O(n) [more like n/2]
 # Space Complexity: O(1) constant. Only storing j and j
+# Doesn't handle case when there are multiple special characters
 def isPalindromeOptimized(s):
-    if len(s) < 2:
+    if len(s) < 2 and s[0].isalnum() == False:
+        return False
+    else: 
         return True
     i, j = 0, len(s) - 1
     while (i <= j):
@@ -41,4 +22,5 @@ def isPalindromeOptimized(s):
 true = "A man, a plan, a canal : Panama"
 false = "race e car! d"
 tacocat = "tacocat"
-print (isPalindromeOptimized(false))
+lam = "!"
+print (isPalindromeOptimized(lam)
