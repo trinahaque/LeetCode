6aad227b21da76b9f5572064befac230c44dca73
trina <trina_haque28@outlook.com>
2018-10-22 23:46:24 -0700

    Edge cases in singly linked list

--- Easy/LinkedList/reverseList.py
+++ Easy/LinkedList/reverseList.py
@@ -4,28 +4,10 @@ Class ListNode:
         self.next = None
 
 Class Solution:
-    def reverseList(self, head):
-        if head == None:
-            return None
-        elif head.next == None:
-            return head
-        else:
-            prev = None
-            current = head
-            next = head.next
-            
-            while next:
-                current.next = prev
-                prev = current
-                current = next
-                next = next.next
-
-            current.next = prev
-            return current
-    
     # Reverse a singly linked list iteratively
     # Time Complexity: O(n) where n is the number of nodes
     # Space Complexity: O(1)
+    # Handles edge cases such as head is none or only one node in the list
     def reverseListRefactored(self, head):
         prev = None
         current = head
