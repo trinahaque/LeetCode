3fed6d95c4d347c65adbdf483cab9b5cdf319a3e
trina <trina_haque28@outlook.com>
2019-03-19 16:56:30 -0700

    stock price i

--- Easy/Array/stockMaxProfit.py
+++ Easy/Array/stockMaxProfit.py
@@ -22,8 +22,32 @@ def maxProfit(prices):
 # 2) Array can be empty
 # 3) Array can have one value, it will return profit = 0 in that case
 
-
 nums = [7, 1, 5, 3, 6, 4]
 nums2 = [7, 6, 4, 3, 1, 8, 12]
-
 print(maxProfit(nums2))
+
+# March 19
+# TimeComplexity: O(n) --> one for loop
+# SpaceComplexity: O(1) or constant 
+
+def stockPriceI(prices):
+    # empty array
+    profit = 0
+    buy = 0
+    if len(prices) < 2:
+        return profit
+
+    for i in range(1, len(prices)):
+        # update profit only if it's greater than previous profit
+        # it handles cases where prices go down
+        if prices[i] - prices[buy] > profit:
+            profit = prices[i] - prices[buy]
+        elif prices[buy] > prices[i]:
+            buy = i
+    return profit
+
+prices = [2, 3, 2, 7, 1]
+print (stockPriceI(prices))
+        
+
+        
