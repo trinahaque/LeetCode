02b67b40495206ce358420010b7ce5f2aaf39124
trina <trina_haque28@outlook.com>
2019-03-25 18:33:26 -0700

    Add node to a singly linked list to the back

---
+++ Easy/LinkedList/addNode.py
@@ -0,0 +1,33 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+
+    # Time Complexity: O(n) --> iterate through all nodes
+    # Space Complexity: Constant --> only variable current created
+    def addLast(self, val):
+        newNode = Node(val)
+        if self.head == None:
+            self.head = newNode
+            return
+        else:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = newNode
+            return
+
+    def print(self):
+        current = self.head
+        while current:
+            print (current.val)
+            current = current.next
+
+sll = SinglyLinkedList()
+sll.addLast(1)
+sll.addLast(5)
+sll.print()
