140d205497be2dd8f533326094784a28ba1eb449
trina <trina_haque28@outlook.com>
2019-03-25 22:35:32 -0700

    solve cycle in a linked list

--- Easy/LinkedList/hasCycle.py
+++ Easy/LinkedList/hasCycle.py
@@ -12,13 +12,34 @@ class Solution(object):
     def hasCycle(self, head):
         if head == None or head.next == None:
             return False
-        
         # Slow has to be the first value. Otherwise fast will catch up to slow even though 
         # there is no cycle
         slow = head
         fast = head.next
 
         while slow != fast:
+            if fast == None or fast.next == None:
+                return False
+            slow = slow.next
+            fast = fast.next.next
+        return True
+
+# Time Complexity: O(n) --> categorically
+# Space Complexity: O(1) --> constant
+def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        # empty list
+        if head == None:
+            return False
+        slow = head
+        fast = head.next
+        
+        # it has cycle if both fast and slow point to the same node
+        while slow != fast:
+            # handles cyclibility with one node cycle/pointing to itself
             if fast == None or fast.next == None:
                 return False
             slow = slow.next
