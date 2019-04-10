341ba6dd49cb94da73af9c8e21af2475d25029c4
trina <trina_haque28@outlook.com>
2019-04-10 15:36:36 -0700

    Rotate an array

--- Easy/Array/rotateArray.py
+++ Easy/Array/rotateArray.py
@@ -1,48 +1,24 @@
-def rotate(nums, k):
-    """
-    Do not return anything, modify nums in-place instead.
-    """
-    k = k % len(nums)
-    i = 0
-    while i < k:
-        nums[i], nums[i+k-len(nums)] = nums[i+k-len(nums)], nums[i]
-        # print ("nums at ", i , " is ", nums)
-        i = i + 1
-    return nums
-
-def rotateArr(arr, r):
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        if len(arr) < 2:
-            return arr
-        reverseArr(arr, 0, len(arr)-r-1)
-        reverseArr(arr, r, len(arr)-1)
-        reverseArr(arr, 0, len(arr)-1)
-        return arr
-        
-    
-def reverseArr(arr, start, end):
-    middle = int((end - start) / 2)
-    
-    for i in range(middle + 1):
-        arr[start + i], arr[end - i] = arr[end - i], arr[start + i]
-    return arr
-
-def reverse(nums):
+# Time Complexity: O(n)
+# Space Complexity: Constant
+def rotateArr(nums, k):
     if len(nums) < 2:
         return nums
-    mid = int(len(nums)/2)
-    for i in range(mid):
-        nums[i], nums[len(nums)-1-i] = nums[len(nums)-1-i], nums[i]
+    r = k % len(nums)
+    reverse(nums, 0, len(nums) - r)
+    reverse(nums, len(nums) - r, len(nums))
+    reverse(nums, 0, len(nums))
+    return nums
+
+# Time Complexity: O(n)/2
+# Space Complexity: Constant
+def reverse(nums, start, end):
+    mid = start + int((end-start)/2)
+    i = 0
+    while (start + i) < mid:
+        nums[start + i], nums[end-1-i] = nums[end-1-i], nums[start + i]
+        i += 1
     return nums
 
 nums = [1, 2, 3, 4, 5, 6]
 nums2 = [5, 6]
-print (reverse(nums))
-# k = 3
-# print(rotate(nums, k))
-
-nums = [-1, -100, 3, 99]
-k = 1
-# print(rotateArr(nums, k))
+rotateArr(nums, 6)
