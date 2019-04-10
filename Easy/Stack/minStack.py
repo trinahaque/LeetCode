a86b16df8a149e6cd921136920a7e81807f6ccb8
trina <trina_haque28@outlook.com>
2019-04-09 20:52:00 -0700

    Create min Stack

---
+++ Easy/Stack/minStack.py
@@ -0,0 +1,56 @@
+class MinStack(object):
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+        self.minVals = []
+        
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: None
+        """
+        self.stack.append(x)
+        if len(self.minVals) == 0:
+            self.minVals.append(x)
+        else:
+            if x <= self.minVals[- 1]:
+                self.minVals.append(x)
+        
+
+    def pop(self):
+        """
+        :rtype: None
+        """
+        popVal = self.stack.pop()
+        if popVal == self.minVals[- 1]:
+            self.minVals.pop()
+
+        
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.stack[- 1]
+        
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        return self.minVals[- 1]
+        
+
+
+minStack = MinStack()
+minStack.push(0)
+minStack.push(1)
+minStack.push(0)
+print (minStack.getMin())
+minStack.pop()
+print (minStack.getMin())
+
