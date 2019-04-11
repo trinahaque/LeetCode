6d6d1d61a9b5204bc94e2b39338d29ad009ff8e6
trina <trina_haque28@outlook.com>
2019-04-10 23:00:31 -0700

    Merge two sorted linked list

--- Easy/LinkedList/mergeTwoLists.py
+++ Easy/LinkedList/mergeTwoLists.py
@@ -2,67 +2,43 @@ class ListNode:
     def __init__(self, val):
         self.val = val
         self.next = None
-
-def mergeTwoLists(self, l1, l2):
-    # l1 and l2 are list nodes
+# Time Complexity: O(l1) + O(l2)--> O(n)
+# Space Complexity: Constant (only head reference/variable is created)
+def mergeTwoLists(l1, l2):
+    """
+    :type l1: ListNode
+    :type l2: ListNode
+    :rtype: ListNode
+    """
     if l1 == None and l2 == None:
         return None
-    elif l1 == None and l2 != None:
+    if l1 == None:
         return l2
-    elif l2 == None and l1 != None:
+    if l2 == None:
         return l1
-    else:
-        if l1.val < l2.val:
-            head = l1
-            l1 = l1.next
-        else:
-            head = l2
-            l2 = l2.next
-        current = head
-        while l1 and l2:
-            if l1.val < l2.val:
-                current.next = l1
-                l1 = l1.next
-            else:
-                current.next = l2
-                l2 = l2.next
-            current = current.next
-        if l1 == None:
-            current.next = l2
-        else:
-            current.next = l1
-        return head
-def mergeSortedList(l1, l2):
-    if l1 == None and l2 == None:
-        return None
-    if l1 == None and l2 != None:
-        return l2
-    if l1 != None and l2 == None:
-        return l1
-    
-    if l1.val <= l2.val:
-        head = l1
-        l1 = l1.next
-    else:
-        head = l2
-        l2 = l2.next
-    
-    current = head
     p = l1
     q = l2
-    while p != None and q != None:
+    if p.val <= q.val:
+        head = p
+        p = p.next
+    else:
+        head = q
+        q = q.next
+    current = head
+    while p and q:
         if p.val <= q.val:
-            current = p
+            current.next = p
             p = p.next
         else:
-            current = q
+            current.next = q
             q = q.next
         current = current.next
-    if q is None:
-        current = p
+    if p == None:
+        current.next = q
     else:
-        current = q
+        current.next = p
     return head
+      
 
 
                 
