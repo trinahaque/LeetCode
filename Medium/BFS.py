ea6eb303ab044b51f43dd419c7c349a04f1716d3
trina <trina_haque28@outlook.com>
2019-04-18 17:34:44 -0700

    Breadth First Search

---
+++ Medium/BFS.py
@@ -0,0 +1,50 @@
+# Vertices are the dots/nodes
+# Edges are the lines
+# Graph Representations
+# Adjacency Matrix
+# Adjacency List
+
+import queue
+
+class Graph:
+    def __init__(self, edges):
+        self.edges = edges
+    def neighbors(self, id):
+        return self.edges[id]
+edges = {
+    "A": ["B", "C"],
+    "B": ["C", "E", "G"],
+    "C": ["D", "F"],
+    "D": ["G"],
+    "E": ["F"],
+    "F": ["G"],
+    "G": [],
+}
+graph = Graph(edges)
+# print ("graph is ", graph)
+# print ("Edge of B is ", graph.neighbors("B"))
+
+
+
+# Time Complexity: O(V + E) --> all the nodes and its connections need to be visited
+# Space Complexity: O(V) --> all nodes need to be added to the queue since they have not visited
+def BFS(graph, start):
+    newQueue = queue.Queue()
+    visited = {}
+    # Enqueue --> put
+    newQueue.put(start)
+    visited[start] = True
+
+    while not newQueue.empty():
+        # Dequeue --> get/first one out
+        current = newQueue.get()
+        print ("Visiting node ", current)
+        # graph.neighbors(node) returns an array
+        for eachEdge in graph.neighbors(current):
+            if eachEdge not in visited:
+                # print (eachEdge)
+                newQueue.put(eachEdge)
+                visited[eachEdge] = True
+BFS(graph, "A")
+
+
