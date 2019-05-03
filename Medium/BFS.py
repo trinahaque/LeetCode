60a088c533ac009cd1bbfc6e2c9285bee0020e63
trina <trina_haque28@outlook.com>
2019-05-02 17:21:12 -0700

    Breadth first search with new graph

--- Medium/BFS.py
+++ Medium/BFS.py
@@ -49,4 +49,30 @@ BFS(graph, "A")
 
 # A, B, C, E, G, D, F, 
 
+# BFS --> queue, visit all node's (vertex's) neighbors before moving in to next node
+def BFS2(graph, start):
+    queue2 = queue.Queue()
+    visited = {}
+    visited[start] = True
+    # put is enqueue
+    queue2.put(start)
+    while not queue2.empty():
+        current = queue2.get()
+        print ("Visiting node ", current)
+        for eachVal in graph.neighbors(current):
+            if eachVal not in visited:
+                queue2.put(eachVal)
+                visited[eachVal] = True
+
+edges2 = {
+    "0": ["1", "2"],
+    "1": ["2"],
+    "2": ["3"],
+    "3": ["4"],
+    "4": ["1", "5"],
+    "5": [],
+}
+graph2 = Graph(edges2)
+BFS2(graph2, "0")
+
 
