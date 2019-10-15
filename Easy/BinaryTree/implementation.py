d9d1fa18815d059c7f94f8b27eb30e8e8b8518df
trina <trina_haque28@outlook.com>
2019-10-14 17:12:22 -0700

    Constructed Binary Tree Class

---
+++ Easy/BinaryTree/implementation.py
@@ -0,0 +1,15 @@
+class Tree:
+    def __init__(self):
+        self.root = None
+
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+
+tree = Tree()
+print (tree)
+node = TreeNode(2)
+print (node)
