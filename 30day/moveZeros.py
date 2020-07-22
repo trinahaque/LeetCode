2a545dd24ddda080638825f93069b2ccfe90fa57
trinahaque <trina_haque28@outlook.com>
2020-07-22 15:33:23 -0700

    Move zeros to end of array

--- 30day/moveZeros.py
+++ 30day/moveZeros.py
@@ -1,20 +1,18 @@
+# Time Complexity: O(n) --> two loops
+# Space Compexity: O(1) or constant --> in place swap
 
+# Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
 def moveZeroes(nums):
-    # p is a pointer to 0
-    p = 1
-    # q is a pointer to non zero
-    q = 0
+    nonZeroIndex = 0
+    # find all the non zero numbers and move them to the front
     for i in range(len(nums)):
-        if nums[i] == 0 and p < q:
-            nums[i], nums[q] = nums[q], nums[i]
-        for j in range((i+1), len(nums)):
-            if nums[j] == 0:
-                p = j
-                break
-        for k in range((i+1), len(nums)):
-            if nums[k] != 0:
-                q = j
-                break
+        if nums[i] != 0:
+            nums[nonZeroIndex] = nums[i]
+            nonZeroIndex += 1
+    # nonZeroIndex should point to the index where the first zero should be
+    # add all the zeros to the end of the array
+    for i in range(nonZeroIndex, len(nums)):
+        nums[i] = 0
     return nums
-nums = [1, 0, 0, 3, 12]
+nums = [0, 0, 5, 4, 6]
 print(moveZeroes(nums))
