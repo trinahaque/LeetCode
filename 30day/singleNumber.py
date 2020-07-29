171d4df5aed27cba50b1e0ad0604c7114b913513
trinahaque <trina_haque28@outlook.com>
2020-07-29 15:31:43 -0700

    Single Number

---
+++ 30day/singleNumber.py
@@ -0,0 +1,18 @@
+#Time Complexity --> O(n) --> 2 loops, O(n) + O(n)
+#Space Complexity --> O(n) --> 1 Hash map
+def singleNumber(nums):
+    frequencyDict = {}
+    for number in nums:
+        if number in frequencyDict:
+            frequencyDict[number] += 1
+        else:
+            frequencyDict[number] = 1
+    for key in frequencyDict:
+        if frequencyDict[key] == 1:
+            return key
+
+nums = [2, 2, 1]
+print(singleNumber(nums))
+
+nums = [4, 1, 2, 1, 2]
+print(singleNumber(nums))
