026faf4b85bc470d64a4a6d38e13e4fdbda3d04e
trinahaque <trina_haque28@outlook.com>
2020-10-21 14:51:54 -0700

    Solved valid parenthesis string

---
+++ 30day/validParenthesisString.py
@@ -0,0 +1,34 @@
+def checkValidString(s):
+    openStack = []
+    starStack = []
+    for i in range(len(s)):
+        if s[i] == "(":
+            openStack.append(i)
+        elif s[i] == "*":
+            starStack.append(i)
+        else:
+            if len(openStack) > 0:
+                openStack.pop()
+            elif len(starStack) > 0:
+                starStack.pop()
+            # means both open and star stacks are empty
+            else:
+                return False
+
+    # traversed through the whole string
+    # check if openStack still has some (
+    while len(openStack) > 0:
+        # this means more ( without any match
+        if len(starStack) == 0:
+            return False
+        # * index comes before ( index
+        elif openStack[len(openStack)-1] < starStack[len(starStack)-1]:
+            openStack.pop()
+            starStack.pop()
+        else:
+            return False
+    return True
+
+s = "()"
+print(checkValidString(s))
+                
