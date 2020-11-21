80f4dcd5aa8c5b48fa76999f8d60655a7f8af471
trinahaque <trina_haque28@outlook.com>
2020-11-20 17:48:37 -0800

    Solved house robber problem

--- Easy/Array/houseRobber.py
+++ Easy/Array/houseRobber.py
@@ -1,12 +1,14 @@
+# Time Complexity: O(n) --> one for loop
+# Space Complexity: O(1) or constant --> values change in place
 def rob(nums):
-    if len(nums) == 1:
+    if len(nums) < 1:
+        return 0
+    if len(nums) < 2:
         return nums[0]
-    if len(nums) == 2:
-        return max(nums)
- 
+    nums[1] = max(nums[0], nums[1])
     for i in range(2, len(nums)):
-        nums[i] = Math.max(nums[i-1], nums[i]+nums[i-2])
-    return max(nums)
+        nums[i] = max(nums[i-1], nums[i]+nums[i-2])
+    return nums[len(nums)-1]
 
-nums = [2, 7]
+nums = [1, 0, 0, 2, 5, 100]
 print(rob(nums))
